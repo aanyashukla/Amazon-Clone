b9f94a5b6b93143ee97c127adb55c2b8fe59a7c7
Aanya <aanyashukla67@gmail.com>
2024-10-15 20:56:31 +0530

    Learnt and converted code to promises.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,11 +1,51 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 import {loadProducts} from "../data/products.js";
+import {loadCart} from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve(); //resolve lets us decide when to go to the next step.
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve(); //resolve lets us decide when to go to the next step.
+  });
+
+}).then(() => {
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  }); 
+  
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+
+/*
 loadProducts(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
+// if we have lots of callbacks, our code will become more and more nested. All the indents will make our code hard to work with and hence we use promise. Promises keep our code more flat.
 
